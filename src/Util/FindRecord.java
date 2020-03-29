3dee4d7e0385265497a56af4c32136d16b5b21e2
Muzi Qiu <mu_qiu@encs.concordia.ca>
2020-03-28 22:53:26 -0400

    find record from file using javarandomfile

---
+++ src/Util/FindRecord.java
@@ -0,0 +1,22 @@
+package Util;
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.charset.StandardCharsets;
+
+public class FindRecord {
+
+    public static String findRecordFromFile(String filePath, int lineNum) throws IOException {
+        RandomAccessFile file = new RandomAccessFile(filePath, "r");
+        long position = (lineNum-1) * 100;
+        file.seek(position);
+        byte[] bytes = new byte[100];
+        file.read(bytes);
+        file.close();
+
+        String string = new String(bytes, StandardCharsets.UTF_8);
+        System.out.println("record: " + string);
+        return string;
+    }
+
+}
