78c4c9c590ba2de99219e68c938b4e0f44a72621
wujq95 <wujq1214@mails.jlu.edu.cn>
2020-03-30 09:10:45 -0400

    add junit test

--- src/Util/LineNum.java
+++ src/Util/LineNum.java
@@ -5,18 +5,22 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
-public class getLineNum {
+public class LineNum {
 
     public static void main(String[] args) throws IOException {
         String path = "/Users/wujiaqi/IdeaProjects/COMP6521_LA2/bitmap.txt";
+        System.out.println(getLineNum(path));
+    }
+
+    public static int getLineNum(String path) throws IOException {
         FileReader fileReader = new FileReader(path);
         BufferedReader bufferedReader = new BufferedReader(fileReader);
         int num=0;
         while(bufferedReader.readLine()!=null){
             num++;
         }
-        System.out.println(num);
         bufferedReader.close();
         fileReader.close();
+        return num;
     }
 }
