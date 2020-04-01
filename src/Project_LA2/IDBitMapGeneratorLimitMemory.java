295c75358183bd8eef0b4b4d9054a00078d63ca8
wujq95 <wujq1214@mails.jlu.edu.cn>
2020-04-01 07:44:48 -0400

    change notation

--- src/Project_LA2/IDBitMapGeneratorLimitMemory.java
+++ src/Project_LA2/IDBitMapGeneratorLimitMemory.java
@@ -24,63 +24,8 @@ public class IDBitMapGeneratorLimitMemory {
 
         FileReader fileReader = new FileReader(inputPath);
         BufferedReader bufferedReader = new BufferedReader(fileReader);
-        int st=0;
 
         for(int q =100;q<150;q++){
-            /*FileWriter fileWriter =new FileWriter(outputPath+q+".txt");
-            PrintWriter printWriter = new PrintWriter(fileWriter);
-            HashMap<String, HashSet<Integer>> map= new HashMap<>();
-            int num = 0;
-            String str = null;
-            //store the data file information in the map
-            while((str=bufferedReader.readLine())!=null&&num<=lineNum){
-                num++;
-                String id = str.substring(0,8);
-                //the key is id, the value is line num
-                if(!map.containsKey(id)){
-                    HashSet<Integer> set = new HashSet<>();
-                    set.add(num);
-                    map.put(id,set);
-                }else{
-                    HashSet set = map.get(id);
-                    set.add(num);
-                    map.put(id,set);
-                }
-            }
-
-            //output the bitmap index file
-            for(int a1=0;a1<10;a1++){
-                for(int a2=0;a2<10;a2++){
-                    for(int a3=0;a3<10;a3++){
-                        for(int a4=0;a4<10;a4++){
-                            for (int a5=0;a5<10;a5++){
-                                for (int a6=0;a6<10;a6++){
-                                    for (int a7=0;a7<10;a7++){
-                                        for (int a8=0;a8<10;a8++){
-                                            String opt = ""+a1+a2+a3+a4+a5+a6+a7+a8;
-                                            //check existing the id
-                                            if(map.containsKey(opt)){
-                                                StringBuffer sb = new StringBuffer();
-                                                HashSet resSet = map.get(opt);
-                                                for(int p=1;p<=num;p++){
-                                                    if(resSet.contains(p)){
-                                                        sb.append(1);
-                                                    }else{
-                                                        sb.append(0);
-                                                    }
-                                                }
-                                                //print a single line
-                                                printWriter.println(opt+sb.toString());
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }*/
-
             FileWriter fileWriter =new FileWriter(outputPath+q+".txt");
             PrintWriter printWriter = new PrintWriter(fileWriter);
             HashMap<Integer, HashSet<Integer>> map= new HashMap<>();
@@ -88,7 +33,6 @@ public class IDBitMapGeneratorLimitMemory {
             String str = null;
             while(( (num<lineNum)&&(str=bufferedReader.readLine())!=null)){
                 num++;
-                st++;
                 Integer id = Integer.parseInt(str.substring(0,8));
                 //the key is id, the value is line num
                 if(!map.containsKey(id)){
@@ -116,12 +60,9 @@ public class IDBitMapGeneratorLimitMemory {
                     printWriter.println(sb.toString());
                 }
             }
-            System.out.println(num);
             printWriter.close();
             fileWriter.close();
         }
-
-        System.out.println(st);
         bufferedReader.close();
         fileReader.close();
     }
