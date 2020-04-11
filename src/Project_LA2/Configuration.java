060551cc9e914481aa410920cc3348a2ad2aff94
Muzi Qiu <mu_qiu@encs.concordia.ca>
2020-04-10 22:26:37 -0400

    add original size report
    fix

--- src/Project_LA2/Configuration.java
+++ src/Project_LA2/Configuration.java
@@ -11,10 +11,11 @@ public class Configuration {
     public static String path1 = "/Users/wujiaqi/comp6521/smallfile/sameple3.txt";
     public static String path2 = "/Users/wujiaqi/comp6521/smallfile/sameple4.txt";
     public static String tempContent = "/Users/wujiaqi/comp6521/";
-    public static String tempContent2 = tempContent+"o";
+    public static String tempContent2 = tempContent + "o";
     public static String idOutput = "/Users/wujiaqi/comp6521/output.txt";
     public static String sexOutput = "src/Data_Files/sexBitMapOutput";
     public static String dptOutput = "src/Data_Files/dptBitMapOutput";
     public static String compressedOutput = "/Users/okakiko/COMP6521_LA2/src/Data_Files/compressed";
-    
+    public static String empIdOutput = "/Users/okakiko/COMP6521_LA2/src/Data_Files/temp";
+
 }
--- src/Test/CompressBitmapTest.java
+++ src/Test/CompressBitmapTest.java
@@ -13,42 +13,55 @@ public class CompressBitmapTest {
     @Test
     public void testCompressBitmap() throws IOException {
         //compress sex bitmap
-        String inputPathSex = Configuration.sexOutput+"1.txt";
-        String outputPathSex = Configuration.compressedOutput + "/compressedSex.txt";
-        long startTimeSex = new Date().getTime();
-        CompressBitmap.compressBitmap(inputPathSex, outputPathSex);
-        long endTimeSex = new Date().getTime();
+        for (int i = 1; i < 3; i++) {
+            String inputPathSex = Configuration.sexOutput + i + ".txt";
+            String outputPathSex = Configuration.compressedOutput + "/compressedSex.txt";
+            long startTimeSex = new Date().getTime();
+            CompressBitmap.compressBitmap(inputPathSex, outputPathSex);
+            long endTimeSex = new Date().getTime();
 
-        File originalFile1 = new File(inputPathSex);
-        File file1 = new File(outputPathSex);
+            File originalFile1 = new File(inputPathSex);
+            File file1 = new File(outputPathSex);
 
-        System.out.println("the compressed of sex bitmap costs " + (endTimeSex - startTimeSex) + " milliseconds, " +
-                "the compressed file size is " + file1.length() + " the original size is " + originalFile1.length());
+            System.out.println("the compressed of sex bitmap " + i + " costs " + (endTimeSex - startTimeSex) + " milliseconds, " +
+                    "the compressed file size is " + file1.length() + " the original size is " + originalFile1.length());
+        }
 
         //compress empID bitmap
-        String inputPathEmp = Configuration.idOutput;
-        String outputPathEmp = Configuration.compressedOutput + "/compressedEmpId.txt";
+        long empIdBitmapSize = 0;
+        long empIdCompressedBitmapSize = 0;
         long startTimeEmp = new Date().getTime();
-        CompressBitmap.compressBitmap(inputPathEmp, outputPathEmp);
+        int smallfileNum = 4;
+        for (int i = 0; i < smallfileNum; i++) {
+            String inputPathEmp = Configuration.empIdOutput + i + ".txt";
+            String outputPathEmp = Configuration.compressedOutput + "/compressedEmpId" + i + ".txt";
+            CompressBitmap.compressBitmap(inputPathEmp, outputPathEmp);
+
+            File file2 = new File(outputPathEmp);
+            empIdCompressedBitmapSize = empIdCompressedBitmapSize + file2.length();
+            File originalFile2 = new File(inputPathEmp);
+            empIdBitmapSize = empIdBitmapSize + originalFile2.length();
+        }
         long endTimeEmp = new Date().getTime();
 
-        File file2 = new File(outputPathEmp);
-        File originalFile2 = new File(inputPathEmp);
-
         System.out.println("the compressed of emp bitmap costs " + (endTimeEmp - startTimeEmp) + " milliseconds, " +
-                "the compressed file size is " + file2.length() + " the original size is " + originalFile2.length());
+                "the compressed file size is " + empIdCompressedBitmapSize + " the original size is " + empIdBitmapSize);
 
         //compress department bitmap
-        String inputPathDpt = Configuration.dptOutput+"1.txt";
-        String outputPathDpt = Configuration.compressedOutput + "/compressedDptId.txt";
-        long startTimeDpt = new Date().getTime();
-        CompressBitmap.compressBitmap(inputPathDpt, outputPathDpt);
-        long endTimeDpt = new Date().getTime();
 
-        File file3 = new File(outputPathDpt);
-        File originalFile3 = new File(inputPathDpt);
+        for (int i = 1; i < 3; i++) {
 
-        System.out.println("the compressed of department bitmap costs " + (endTimeDpt - startTimeDpt) + " milliseconds, " +
-                "the compressed file size is " + file3.length() + " the original size is " + originalFile3.length());
+            String inputPathDpt = Configuration.dptOutput + i + ".txt";
+            String outputPathDpt = Configuration.compressedOutput + "/compressedDptId.txt";
+            long startTimeDpt = new Date().getTime();
+            CompressBitmap.compressBitmap(inputPathDpt, outputPathDpt);
+            long endTimeDpt = new Date().getTime();
+
+            File file3 = new File(outputPathDpt);
+            File originalFile3 = new File(inputPathDpt);
+
+            System.out.println("the compressed of department bitmap " + i + " costs " + (endTimeDpt - startTimeDpt) + " milliseconds, " +
+                    "the compressed file size is " + file3.length() + " the original size is " + originalFile3.length());
+        }
     }
 }
